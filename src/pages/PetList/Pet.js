a4f3fa59db2d676bae8c84bae91e1fee92a14a5d
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-01 09:55:39 +0900

    feat: 반려견 상세정보 확인 기능 구현

--- src/pages/PetList/Pet.js
+++ src/pages/PetList/Pet.js
@@ -1,10 +1,40 @@
 import React from "react";
 import './Pets.css'
+
 function Pet({pet, onClick}) {
+  const simple =       
+  <>
+    <img className="pet-img" src={pet.img} />
+    <p>{pet.name} / {pet.species} / {pet.birthday}</p>
+  </>
+
+  const detail = 
+  <>
+    <div className="pet-first">
+      <div>
+        <img className="pet-img" src={pet.img} />
+        <div className="pet-group">
+          <button>그룹관리</button>
+          <p>초대코드 : ABCD</p>
+          <p>맴버1</p>
+          <p>맴버2</p>
+          <button>그룹 나가기</button>
+        </div>
+      </div>
+      <div className="pet-info">
+        <p>이름 {pet.name}</p>
+        <p>견종 {pet.species}</p>
+        <p>생일 {pet.birthday}</p>
+      </div>
+    </div>
+    <div className="pet-second">
+      <button>사회화</button>
+      <button>건강수첩</button>
+    </div>
+  </>
   return (
-    <div className="pet-block" onClick={onClick}>
-      <img className="pet-img" src={pet.img} />
-      <p>{pet.name} / {pet.species} / {pet.birthday}</p>
+    <div className={`${!pet.isClick ? "pet-block" : "pet-detail"}`} onClick={()=>onClick(pet.id)}>
+      {!pet.isClick ? simple : detail}
     </div>
   )
 }
--- src/pages/PetList/PetList.js
+++ src/pages/PetList/PetList.js
@@ -3,6 +3,7 @@ import Pet from './Pet';
 import './Pets.css';
 
 function PetList() {
+  const [toggle, setToggle] = useState(false);
   const [pets, setPets] = useState([
     {
       id: 0,
@@ -10,6 +11,7 @@ function PetList() {
       species: "진돗개",
       birthday: "2023.04.30",
       img: "https://img.freepik.com/premium-photo/little-fluffy-puppy-of-pomeranian-spitz-lying-on-bright-yellow-background_253512-22.jpg",
+      isClick: false,
     },
     {
       id: 1,
@@ -17,6 +19,7 @@ function PetList() {
       species: "진돗개",
       birthday: "2023.04.30",
       img: "https://image.dongascience.com/Photo/2022/06/6982fdc1054c503af88bdefeeb7c8fa8.jpg",
+      isClick: false,
     },
     {
       id: 2,
@@ -24,14 +27,19 @@ function PetList() {
       species: "진돗개",
       birthday: "2023.04.30",
       img: "https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081724428qquq.jpg",
+      isClick: false,
     },
   ]);
+
+  const onClick = (id) => {
+    setPets(pets.map(pet=>pet.id===id?{ ...pet, isClick: !pet.isClick }: pet));
+  }
 
   return (
     <>
       <div className="list_container">
         {pets.map(pet => (
-          <Pet pet={pet} key={pet.id} />
+          <Pet pet={pet} key={pet.id} onClick={onClick}/>
         ))}
       </div>
     </>
