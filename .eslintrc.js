d9efd2ae55c4db454840d8327915fb6863cd76bd
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-02 01:24:53 +0900

    chore: eslint 설치 및 rules 설정

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV==='production'?'error':'off',
+    'linebreak-style': 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/prop-types": "off",
+    'react/jsx-one-expression-per-line': 0,
+    'arrow-body-style': "off",
+    'react/button-has-type': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+  },
+};
