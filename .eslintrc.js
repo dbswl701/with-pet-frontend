dcb1c88ef755d5ca9593a0ecaa5ff955143cd804
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-02 20:42:44 +0900

    fix: eslint로 인한 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,5 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'react/jsx-no-useless-fragment': 'off',
-    'prefer-arrow-callback': 0,
   },
 };
