417d48efbf5a44f8af42ab4591a6ab5c4e5f6c07
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-03 09:41:34 +0900

    design: 반려견 리스트 클릭 시 UI 디자인 변경

--- src/pages/PetList/Pet.js
+++ src/pages/PetList/Pet.js
@@ -2,42 +2,41 @@ import React from 'react';
 import './Pets.css';
 
 function Pet({ pet, onClick }) {
-  const simple = () => {
-    return (
-      <>
-        <img className="pet-img" src={pet.img} alt="반려견 프로필 사진" />
-        <p>{pet.name} / {pet.species} / {pet.birthday}</p>
-      </>
-    );
-  };
-
-  const detail = () => {
-    return (
-      <>
-        <div className="pet-first">
-          <div>
-            <img className="pet-img" src={pet.img} alt="반려견 프로필 사진" />
-            <div className="pet-group">
-              <button>그룹관리</button>
-              <p>초대코드 : ABCD</p>
-              <p>맴버1</p>
-              <p>맴버2</p>
-              <button>그룹 나가기</button>
-            </div>
-          </div>
-          <div className="pet-info">
-            <p>이름 {pet.name}</p>
-            <p>견종 {pet.species}</p>
-            <p>생일 {pet.birthday}</p>
+  const simple = (
+    <>
+      <img className="pet-img" src={pet.img} alt="반려견 프로필 사진" />
+      <p>{pet.name} / {pet.breed} / {pet.birthday}</p>
+    </>
+  );
+  const detail = (
+    <>
+      <div className="pet-first">
+        <div className="pet-img-group">
+          <img className="pet-img" src={pet.img} alt="반려견 프로필 사진" />
+          <div className="pet-group">
+            <button>그룹관리</button>
+            <p>초대코드 : ABCD</p>
+            <p>맴버1</p>
+            <p>맴버2</p>
+            <button>그룹 나가기</button>
           </div>
         </div>
-        <div className="pet-second">
-          <button>사회화</button>
-          <button>건강수첩</button>
+        <div className="pet-info">
+          <h1>이름 {pet.name}</h1>
+          <p>견종 {pet.breed}</p>
+          <p>생일 {pet.birthday}</p>
+          <p>성별 {pet.gender}</p>
+          <p>중성화 여부 {pet.neutralization}</p>
+          <p>무게 {pet.weight}</p>
+          <p>등록코드 {pet.isbn}</p>
         </div>
-      </>
-    );
-  };
+      </div>
+      <div className="pet-second">
+        <button>사회화</button>
+        <button>건강수첩</button>
+      </div>
+    </>
+  );
 
   return (
     <div className={`${!pet.isClick ? 'pet-block' : 'pet-detail'}`} onClick={() => onClick(pet.id)}>
