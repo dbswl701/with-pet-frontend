4bbea6ae5fa0228970949b0c22795b39de7aa2a5
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-05 23:10:42 +0900

    feat: 반려견 정보 수정 API 연동

--- src/pages/PetList/PetModify.js
+++ src/pages/PetList/PetModify.js
@@ -3,15 +3,14 @@ import dogimgdefault from '../../assets/dogProfileImage.png';
 
 function PetModify({ onSubmit, petInfo, setIsModify }) {
   const [modifyPetInfo, setModifyPetInfo] = useState({
-    dog_id: petInfo.id,
-    dog_name: petInfo.name,
-    dog_breed: petInfo.breed,
-    dog_birth: petInfo.birthday,
-    dog_gender: petInfo.gender,
+    dog_name: petInfo.dog_name,
+    dog_breed: petInfo.dog_breed,
+    dog_birth: petInfo.dog_birthday,
+    dog_gender: petInfo.dog_gender,
     neutralization: petInfo.neutralization,
-    dog_weight: petInfo.weight,
-    dog_img: petInfo.img,
-    dog_isbn: petInfo.isbn,
+    dog_weight: petInfo.dog_weight,
+    dog_img: petInfo.dog_img,
+    dog_isbn: petInfo.dog_isbn,
   });
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -24,21 +23,21 @@ function PetModify({ onSubmit, petInfo, setIsModify }) {
   const onLocalSubmit = (e) => {
     e.preventDefault();
     setIsModify((prev) => !prev);
-    onSubmit(petInfo.id, modifyPetInfo);
+    onSubmit(petInfo.dog_id, modifyPetInfo);
   };
 
   const modify = (
     <form onSubmit={onLocalSubmit}>
       <div className="pet-img-regist">
-        <img id="preview-image" alt="이미지 미리보기" src={!modifyPetInfo.img ? dogimgdefault : modifyPetInfo.img} />
+        <img id="preview-image" alt="이미지 미리보기" src={!modifyPetInfo.dog_img ? dogimgdefault : modifyPetInfo.dog_img} />
         <label htmlFor="image-select">프로필 이미지 선택</label>
         <input type="file" accept="image/*" id="image-select" style={{ display: 'none' }} onChange={onChange} />
       </div>
       <div className="pet-info-regist">
-        <input type="text" name="name" placeholder="반려견 이름을 입력해주세요." onChange={onChange} value={modifyPetInfo.name} required />
+        <input type="text" name="dog_name" placeholder="반려견 이름을 입력해주세요." onChange={onChange} value={modifyPetInfo.dog_name} required />
 
         <label htmlFor="breeds">견종</label>
-        <input type="select" list="list" id="breeds" name="breed" onChange={onChange} value={modifyPetInfo.breed} />
+        <input type="select" list="list" id="breeds" name="dog_breed" onChange={onChange} value={modifyPetInfo.dog_breed} />
         <datalist id="list">
           <option value="진돗개" />
           <option value="삽살개" />
@@ -47,13 +46,13 @@ function PetModify({ onSubmit, petInfo, setIsModify }) {
         </datalist>
 
         <label htmlFor="birthday">생일</label>
-        <input type="date" name="birthday" id="birthday" onChange={onChange} value={modifyPetInfo.birthday} required />
+        <input type="date" name="dog_birthday" id="birthday" onChange={onChange} value={modifyPetInfo.dog_birth} required />
 
         <div className="select">
           <p>성별 선택</p>
-          <input type="radio" name="gender" id="male" value="male" onChange={onChange} checked={modifyPetInfo.gender === 'male'} />
+          <input type="radio" name="dog_gender" id="male" value="male" onChange={onChange} checked={modifyPetInfo.dog_gender === 'male'} />
           <label htmlFor="male">남자</label>
-          <input type="radio" name="gender" id="female" value="female" onChange={onChange} checked={modifyPetInfo.gender === 'female'} />
+          <input type="radio" name="dog_gender" id="female" value="female" onChange={onChange} checked={modifyPetInfo.dog_gender === 'female'} />
           <label htmlFor="female">여자</label>
         </div>
 
@@ -66,10 +65,10 @@ function PetModify({ onSubmit, petInfo, setIsModify }) {
         </div>
 
         <label htmlFor="weight">무게</label>
-        <input type="number" name="weight" id="weight" min="0" value={modifyPetInfo.weight} onChange={onChange} />
+        <input type="number" name="dog_weight" id="weight" min="0" value={modifyPetInfo.dog_weight} onChange={onChange} />
 
         <label htmlFor="isbn">등록코드</label>
-        <input type="text" name="isbn" placeholder="등록코드를 입력하세요(-제외)." required value={modifyPetInfo.isbn} onChange={onChange} />
+        <input type="text" name="dog_isbn" placeholder="등록코드를 입력하세요(-제외)." required value={modifyPetInfo.dog_isbn} onChange={onChange} />
         <input type="submit" value="submit" />
         {/* <input type="button" value="cancel" onClick={() => { setisClick(false); }} /> */}
       </div>
