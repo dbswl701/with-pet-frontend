2adf514395f8bfdc1983eab79fe795f43638ee3c
Jihyun Lee <wlgus97@ajou.ac.kr>
2023-05-07 20:33:44 +0900

    💡comment: 필요 없는 주석 삭제

--- src/pages/MainPage/Region.js
+++ src/pages/MainPage/Region.js
@@ -19,21 +19,6 @@ const regionList = [
   { region: '원문동', city: '과천' },
 ];
 
-// const GroupHeader = styled('div')(({ theme }) => ({
-//   position: 'sticky',
-//   top: '-8px',
-//   padding: '4px 10px',
-//   color: theme.palette.primary.main,
-//   backgroundColor:
-//     theme.palette.mode === 'light'
-//       ? lighten(theme.palette.primary.light, 0.85)
-//       : darken(theme.palette.primary.main, 0.8),
-// }));
-
-// const GroupItems = styled('ul')({
-//   padding: 0,
-// });
-
 export default function Asynchronous() {
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState([]);
