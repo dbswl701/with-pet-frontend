4beda0dd62f8b5a95dad08f3897b7db57f034430
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-05-08 19:21:26 +0900

    ➕ add: 회원정보 수정에 선택 이미지 미리보기 추가

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,135 +1,149 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-const url = 'http://ec2-3-39-193-176.ap-northeast-2.compute.amazonaws.com:8080/api/v1/users/my-info';
+const url =
+  "https://d45162fd-d516-4456-83d9-d3b784b62ec2.mock.pstmn.io/api/v1/users/my-info";
 
 function EditProfile() {
+  const [userInfo, setUserInfo] = useState(null);
+  const [imageSrc, setImageSrc] = useState("");
 
-    const [userInfo, setUserInfo] = useState(null);
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+        resolve();
+      };
+    });
+  };
 
-    useEffect(() => {
-        axios.get(url)
-          .then((response) => {
-            setUserInfo(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-            console.log("Editprofile error");
-          });
-      }, []);
-    // 회원정보를 수정하기 위해 입력받을 새로운 상태값
-    const [newPassword, setNewPassword] = useState(userInfo.password);
-    const [newName, setNewName] = useState(userInfo.name);
-    const [newPhone, setNewPhone] = useState(userInfo.phone);
-    const [newAddressPost, setNewAddressPost] = useState(userInfo.zipcode);
-    const [newAddressRoad, setNewAddressRoad] = useState(userInfo.streetAdr);
-    const [newAddressDtail, setNewAddressDtail] = useState(userInfo.detailAdr);
-    const [newEmail, setNewEmail] = useState(userInfo.email);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setUserInfo(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("Editprofile error");
+      });
+  }, []);
 
-    // 현재 회원정보를 불러와 폼에 미리 채워놓는 기능
-    useEffect(() => {
-        setNewPassword(newPassword);
-        setNewName(newName);
-        setNewPhone(newPhone);
-        setNewAddressPost(newAddressPost);
-        setNewAddressRoad(newAddressRoad);
-        setNewAddressDtail(newAddressDtail);
-        setNewEmail(newEmail);
-    }, []);
+  // 회원정보를 수정하기 위해 입력받을 새로운 상태값
+  const [newPassword, setNewPassword] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [newAddressPost, setNewAddressPost] = useState("");
+  const [newAddressRoad, setNewAddressRoad] = useState("");
+  const [newAddressDtail, setNewAddressDtail] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
-    // 입력한 회원정보를 서버로 보내어 수정하는 기능
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const updatedProfile = {
-            password: newPassword,
-            name: newName,
-            phone: newPhone,
-            zipcode: newAddressPost,
-            streetAdr: newAddressRoad,
-            detailAdr: newAddressDtail,
-            email: newEmail,
-        };
-        // 서버로 수정된 회원정보를 보내는 코드
-        axios.put(url, updatedProfile)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  // 현재 회원정보를 불러와 폼에 미리 채워놓는 기능
+
+  // 입력한 회원정보를 서버로 보내어 수정하는 기능
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updatedProfile = {
+      password: newPassword,
+      userName: newName,
+      phoneNum: newPhone,
+      zipcode: newAddressPost,
+      streetAdr: newAddressRoad,
+      detailAdr: newAddressDtail,
+      userEmail: newEmail
     };
+    // 서버로 수정된 회원정보를 보내는 코드
+    axios
+      .put(url, updatedProfile)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-
-        <form onSubmit={handleSubmit}>
-            <h1>Edit Profile</h1>
-
-            <label>
-                비밀번호:
-                <input
-                    type="password"
-                    value={newPassword}
-                    onChange={(event) => setNewPassword(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                이름:
-                <input
-                    type="text"
-                    value={newName}
-                    onChange={(event) => setNewName(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                전화번호:
-                <input
-                    type="tel"
-                    value={newPhone}
-                    onChange={(event) => setNewPhone(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                우편번호:
-                <input
-                    type="text"
-                    value={newAddressPost}
-                    onChange={(event) => setNewAddressPost(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                도로명주소:
-                <input
-                    type="text"
-                    value={newAddressRoad}
-                    onChange={(event) => setNewAddressRoad(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                상세주소:
-                <input
-                    type="text"
-                    value={newAddressDtail}
-                    onChange={(event) => setNewAddressDtail(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                이메일:
-                <input
-                    type="email"
-                    value={newEmail}
-                    onChange={(event) => setNewEmail(event.target.value)}
-                />
-            </label>
-            <br />
-            <button type="submit">Update Profile</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>회원정보 수정</h1>
+      <div className="preview">
+        {imageSrc && <img src={imageSrc} alt="preview-img" />}
+      </div>
+      <input
+        type="file"
+        onChange={(e) => {
+          encodeFileToBase64(e.target.files[0]);
+        }}
+      />
+      <br />
+      <label>
+        비밀번호:
+        <input
+          type="password"
+          // value (newPassword가 비어있을 때, userInfo 객체가 존재하고 그 안에 password 속성이 있으면 해당 값을 사용하도록 설정)
+          value={newPassword || (userInfo && userInfo.password) || ""}
+          onChange={(event) => setNewPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        이름:
+        <input
+          type="text"
+          value={newName || (userInfo && userInfo.userName) || ""}
+          onChange={(event) => setNewName(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        전화번호:
+        <input
+          type="tel"
+          value={newPhone || (userInfo && userInfo.phoneNum) || ""}
+          onChange={(event) => setNewPhone(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        우편번호:
+        <input
+          type="text"
+          value={newAddressPost || (userInfo && userInfo.zipcode) || ""}
+          onChange={(event) => setNewAddressPost(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        도로명주소:
+        <input
+          type="text"
+          value={newAddressRoad || (userInfo && userInfo.streetAdr) || ""}
+          onChange={(event) => setNewAddressRoad(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        상세주소:
+        <input
+          type="text"
+          value={newAddressDtail || (userInfo && userInfo.detailAdr) || ""}
+          onChange={(event) => setNewAddressDtail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        이메일:
+        <input
+          type="email"
+          value={newEmail || (userInfo && userInfo.userEmail) || ""}
+          onChange={(event) => setNewEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Update Profile</button>
+    </form>
+  );
 }
 export default EditProfile;
