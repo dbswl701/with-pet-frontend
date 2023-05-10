9eaae05e302c379fee0bf54af26a7a238bcf0908
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-05-11 00:57:55 +0900

    ✨ feat: UsageList - 예약정보 표시해야 하는 것 까지 정함

--- src/pages/UsageList/UsageList.js
+++ src/pages/UsageList/UsageList.js
@@ -5,15 +5,15 @@ import axios from 'axios';
 const url = 'https://withpet.site/api/v1/users/my-info';
 function UsageList() {
   const [usageHistory, setUsageHistory] = useState([]);
-  // const [petImg, setPetImg] = useState({
-  //   dog_img: '',
-  // });
+  const [setPetImg] = useState({
+    dog_img: '',
+  });
 
   useEffect(() => {
     axios.get(url)
       .then((response) => {
         setUsageHistory(response.data.result);
-        // setPetImg(response.data.result.dog_img);
+        setPetImg(response.data.result.dog_img);
       })
       .catch(() => {
       });
@@ -27,7 +27,7 @@ function UsageList() {
         <p>비용: {usageHistory.totalCost}</p>
         <p>주소: {usageHistory.address.streetAdr} {usageHistory.address.detailAdr} </p>
         <p>옵션: {usageHistory.petsitterOption}</p>
-      </div>  
+      </div>
       <div>반려인 이용내역 페이지</div>
     </>
   );
