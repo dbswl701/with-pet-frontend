089ef4d10a49c6fd4842fad5c0d4ed2fcc87a4b5
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-05-11 17:10:27 +0900

    ✨ feat: 회원가입 사진 추가 시 썸네일 나오도록 추가 흠

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -5,6 +5,7 @@ import './signup.css';
 
 function SignupForm() {
   const navigate = useNavigate();
+  const [imageSrc, setImageSrc] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
@@ -14,6 +15,16 @@ function SignupForm() {
   const [addressPost, setAddressPost] = useState('');
   const [addressDtail, setAddressDtail] = useState('');
   const [email, setEmail] = useState('');
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+        resolve();
+      };
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -56,11 +67,11 @@ function SignupForm() {
       <div>
         <label htmlFor="username">아이디:</label>
         {/* // required 처리하면 됨 */}
-        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" id="username" placeholder="ex) 아이디 입력" value={username} onChange={(e) => setUsername(e.target.value)} />
       </div>
       <div>
         <label htmlFor="password">비밀번호:</label>
-        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" id="password" placeholder="ex) 영문자 + 숫자 + 특수기호" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <div>
         <label htmlFor="passwordConfirm">비밀번호 확인:</label>
@@ -68,15 +79,24 @@ function SignupForm() {
       </div>
       <div>
         <label htmlFor="name">이름:</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" id="name" placeholder="ex) 홍길동" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="preview">
+        {imageSrc && <img src={imageSrc} alt="preview-img" />}
       </div>
       <div>
         <label htmlFor="profile">프로필 사진:</label>
-        <input type="file" accept="image/*" />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            encodeFileToBase64(e.target.files[0]);
+          }}
+        />
       </div>
       <div>
         <label htmlFor="phone">전화번호:</label>
-        <input type="text" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <input type="text" id="phone" placeholder="ex) 010-0000-0000" value={phone} onChange={(e) => setPhone(e.target.value)} />
       </div>
       <div>
         <label htmlFor="address">우편번호:</label>
@@ -88,11 +108,11 @@ function SignupForm() {
       </div>
       <div>
         <label htmlFor="address">상세주소:</label>
-        <input type="text" id="address" value={addressDtail} onChange={(e) => setAddressDtail(e.target.value)} />
+        <input type="text" id="address" placeholder="ex) 건물명 / 동 / 호" value={addressDtail} onChange={(e) => setAddressDtail(e.target.value)} />
       </div>
       <div>
         <label htmlFor="email">E-mail:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" id="email" placeholder="ex) someone@mail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <button type="submit">가입하기</button>
     </form>
