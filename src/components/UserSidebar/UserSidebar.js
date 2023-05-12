9cb52cf49c36a2cd61d8c5c6953656a955ae0c75
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-12 14:30:24 +0900

    ✨ feat: 반려인 캘린더뷰 사이드바 라디오 버튼으로 구현

--- src/components/UserSidebar/UserSidebar.js
+++ src/components/UserSidebar/UserSidebar.js
@@ -1,88 +1,75 @@
 import React, { useState } from 'react';
+import dayjs from 'dayjs';
 import styled from 'styled-components';
-import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 const SideBar = styled.div`
-display: flex;
-background-color: white;
-height: 100vh;
-width: 256px;
-box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px;
-border-radius: 5px;
-margin-top: 50px;
-margin-left: 40px;
-flex-direction: column;
+  display: flex;
+  background-color: white;
+  height: 100vh;
+  width: 256px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px;
+  border-radius: 5px;
+  margin-top: 50px;
+  margin-left: 40px;
+  flex-direction: column;
 `;
 
-function Dog({
-  id, name, isChecked, onChange,
+function Item({
+  item, name, filter, onChange,
 }) {
   return (
     <>
-      <FormControlLabel control={<Checkbox />} label={name} checked={isChecked} onChange={() => onChange(id)} />
+      <div>
+        <input type="radio" name={name} id={item.name} value={item.id} onChange={onChange} checked={filter[name] === item.id} required />
+        <label htmlFor={item.name}>{item.name}</label>
+      </div>
     </>
   );
 }
 
 function UserSideBar() {
-  const [dogs, setDogs] = useState([
-    { id: 0, name: '멍멍이', isChecked: false },
-    { id: 1, name: '멍멍이2', isChecked: false },
-    { id: 2, name: '멍멍이3', isChecked: false },
-  ]);
-  const handleDogChange = (id) => {
-    setDogs((prevDogs) => {
-      const updatedDogs = prevDogs.map((dog) => {
-        if (dog.id === id) {
-          return { ...dog, isChecked: !dog.isChecked };
-        }
-        return dog;
-      });
-      return updatedDogs;
+  const dogs = [
+    { id: '1', name: '멍멍이1' },
+    { id: '2', name: '멍멍이2' },
+    { id: '3', name: '멍멍이3' },
+  ];
+
+  const categories = [
+    { id: '1', name: '산책1' },
+    { id: '2', name: '산책2' },
+    { id: '3', name: '산책3' },
+  ];
+
+  const [filter, setFilter] = useState({
+    dogId: '',
+    category: '',
+    month: dayjs(new Date()).format('YYYY-MM'),
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFilter({
+      ...filter,
+      [name]: value,
     });
   };
 
-  const [categories, setCategories] = useState([
-    { id: 0, name: '산책1', isChecked: false },
-    { id: 1, name: '산책2', isChecked: false },
-    { id: 2, name: '산책3', isChecked: false },
-  ]);
-
-  const handleCategoryChange = (id) => {
-    setCategories((prevDogs) => {
-      const updatedCategories = prevDogs.map((category) => {
-        if (category.id === id) {
-          return { ...category, isChecked: !category.isChecked };
-        }
-        return category;
-      });
-      return updatedCategories;
-    });
-  };
-  console.log(dogs);
-  console.log(categories);
+  console.log(filter);
   return (
     <>
       <SideBar>
         <div style={{ backgroundColor: 'red' }}>
           강아지 선택
-          <FormGroup>
-            {dogs.map((dog) => <Dog key={dog.id} id={dog.id} name={dog.name} isChecked={dog.isChecked} onChange={handleDogChange} />)}
-          </FormGroup>
+          {dogs.map((dog) => <Item key={dog.id} name="dogId" item={dog} filter={filter} onChange={onChange} />)}
         </div>
-        <div style={{ backgroundColor: 'yellow' }}>
-          반려인 / 펫시터 선택
-          <FormGroup>
-            {/* {categories.map((category) => <Dog key={category.id} id={category.id} name={category.name} isChecked={category.isChecked} onChange={handleCategoryChange} />)} */}
-          </FormGroup>
+        <div style={{ backgroundColor: 'red' }}>
+          작성자 선택
+          <button>반려인</button>
+          <button>펫시터</button>
         </div>
-        <div style={{ backgroundColor: 'green' }}>
+        <div style={{ backgroundColor: 'red' }}>
           카테고리 선택
-          <FormGroup>
-            {categories.map((category) => <Dog key={category.id} id={category.id} name={category.name} isChecked={category.isChecked} onChange={handleCategoryChange} />)}
-          </FormGroup>
+          {categories.map((category) => <Item key={category.id} name="category" item={category} filter={filter} onChange={onChange} />)}
         </div>
       </SideBar>
     </>
