ee903bea4217e7af07fd8b847c5cc041def7ec06
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-05-13 23:46:00 +0900

    💄 design: signup.css 지우고 styled-component로 새로 함

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,78 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './signup.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  padding: 50px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 30px;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    label {
+      margin-bottom: 10px;
+      font-weight: bold;
+    }
+
+    input {
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ddd;
+      outline: none;
+      margin-bottom: 20px;
+    }
+
+    input[type='file'] {
+      margin: 20px 0;
+    }
+  }
+
+  .preview {
+    grid-column: 2/3;
+    display: flex;
+    justify-content: center;
+
+    img {
+      max-width: 200px;
+      max-height: 200px;
+      margin-bottom: 20px;
+    }
+  }
+
+  button[type='submit'] {
+    grid-column: 0/3;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: #0077cc;
+    color: #fff;
+    font-size: 1.2rem;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #0065a5;
+    }
+  }
+`;
 
 function SignupForm() {
   const navigate = useNavigate();
@@ -15,6 +86,7 @@ function SignupForm() {
   const [addressPost, setAddressPost] = useState('');
   const [addressDtail, setAddressDtail] = useState('');
   const [email, setEmail] = useState('');
+
   const encodeFileToBase64 = (fileBlob) => {
     const reader = new FileReader();
     reader.readAsDataURL(fileBlob);
@@ -38,84 +110,108 @@ function SignupForm() {
       alert('빈 칸을 모두 입력해주세요.'); // eslint-disable-line no-alert
       return;
     }
-    axios.post('https://withpet.site/api/v1/users/signup', {
-      userId: username,
-      userPassword: password,
-      userName: name,
-      phoneNum: phone,
-      profileImg: '',
-      address: {
-        detailAdr: addressDtail,
-        streetAdr: addressRoad,
-        zipcode: addressPost,
-      },
-      userEmail: email,
-      userPasswordCheck: passwordConfirm,
+    axios.post('https://withpet.herokuapp.com/users/signup', {
+      username,
+      password,
+      name,
+      phone,
+      addressRoad,
+      addressPost,
+      addressDtail,
+      email,
+      image: imageSrc,
     })
-      .then(() => {
-        alert('회원가입에 성공했습니다.'); // eslint-disable-line no-alert
-        navigate('/'); // 회원가입 성공 시 로그인 페이지로 이동
+      .then((res) => {
+        console.log(res);
+        navigate('/login');
       })
-      .catch(() => {
-        alert('회원가입에 실패했습니다.'); // eslint-disable-line no-alert
+      .catch((err) => {
+        console.error(err);
+        alert('회원가입에 실패했습니다. 다시 시도해주세요.'); // eslint-disable-line no-alert
       });
   };
 
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    await encodeFileToBase64(file);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>회원가입</h1>
-      <div>
-        <label htmlFor="username">아이디:</label>
-        {/* // required 처리하면 됨 */}
-        <input type="text" id="username" placeholder="ex) 아이디 입력" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="password">비밀번호:</label>
-        <input type="password" id="password" placeholder="ex) 영문자 + 숫자 + 특수기호" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="passwordConfirm">비밀번호 확인:</label>
-        <input type="password" id="passwordConfirm" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="name">이름:</label>
-        <input type="text" id="name" placeholder="ex) 홍길동" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div className="preview">
-        {imageSrc && <img src={imageSrc} alt="preview-img" />}
-      </div>
-      <div>
-        <label htmlFor="profile">프로필 사진:</label>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => {
-            encodeFileToBase64(e.target.files[0]);
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor="phone">전화번호:</label>
-        <input type="text" id="phone" placeholder="ex) 010-0000-0000" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="address">우편번호:</label>
-        <input type="text" id="address" value={addressPost} onChange={(e) => setAddressPost(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="address">도로명 주소:</label>
-        <input type="text" id="address" value={addressRoad} onChange={(e) => setAddressRoad(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="address">상세주소:</label>
-        <input type="text" id="address" placeholder="ex) 건물명 / 동 / 호" value={addressDtail} onChange={(e) => setAddressDtail(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="email">E-mail:</label>
-        <input type="email" id="email" placeholder="ex) someone@mail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <button type="submit">가입하기</button>
-    </form>
+    <Container>
+      <Title>회원가입</Title>
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">아이디</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="passwordConfirm">비밀번호 확인</label>
+          <input
+            type="password"
+            id="passwordConfirm"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          <label htmlFor="name">이름</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="phone">전화번호</label>
+          <input
+            type="text"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <label htmlFor="addressRoad">주소(도로명)</label>
+          <input
+            type="text"
+            id="addressRoad"
+            value={addressRoad}
+            onChange={(e) => setAddressRoad(e.target.value)}
+          />
+          <label htmlFor="addressPost">주소(우편번호)</label>
+          <input
+            type="text"
+            id="addressPost"
+            value={addressPost}
+            onChange={(e) => setAddressPost(e.target.value)}
+          />
+          <label htmlFor="addressDtail">주소(상세주소)</label>
+          <input
+            type="text"
+            id="addressDtail"
+            value={addressDtail}
+            onChange={(e) => setAddressDtail(e.target.value)}
+          />
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="preview">
+          {imageSrc && <img src={imageSrc} alt="프로필 사진 미리보기" />}
+          <input type="file" onChange={handleImageUpload} />
+        </div>
+        <button type="submit">회원가입</button>
+      </Form>
+    </Container>
   );
 }
 
