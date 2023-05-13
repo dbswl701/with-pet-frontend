8be625426b6d12b523fc3a87d9631a4f06b6f2d3
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-05-13 23:13:21 +0900

    💄 design: styled-component 활용해서 디자인 살짝 바꿈

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,53 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './login.css';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Card = styled.div`
+  width: 400px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+`;
+
+const Button = styled.button`
+  background-color: #731;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  margin-right: 10px;
+  cursor: pointer;
+`;
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -20,41 +66,38 @@ function Login() {
         alert('로그인에 실패했습니다.'); // eslint-disable-line no-alert
       });
   };
+
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button type="submit" className="btn" onClick={onSubmit}>
-              Login
-            </button>
-            <button type="submit" className="btn" onClick={() => navigate('../signup')}>
-              Signup
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Card>
+        <Form>
+          <FormGroup>
+            <Label htmlFor="username">Username</Label>
+            <Input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <Button type="submit" onClick={onSubmit}>
+            로그인
+          </Button>
+          <Button type="submit" onClick={() => navigate('../signup')}>
+            회원가입
+          </Button>
+        </Form>
+      </Card>
+    </Container>
   );
 }
 
