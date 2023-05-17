3a1c5ba65add9884ba893c078b37ec3ce829c32f
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-18 02:03:37 +0900

    ✨ feat: 펫시터 캘린더뷰의 캘린더 구현

--- src/pages/PetsitterCalendar/PetsitterCalendar.js
+++ src/pages/PetsitterCalendar/PetsitterCalendar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CalendarView from './CalendarView';
 import PetsitterSidebar from '../../components/PetsitterSidebar/PetsitterSidebar';
 
 function PetsitterCalendar() {
@@ -6,7 +7,8 @@ function PetsitterCalendar() {
     <>
       <div style={{ display: 'flex' }}>
         <PetsitterSidebar />
-        <div>펫시터 메인페이지(캘린더뷰, 예약 내역 확인)</div>
+        {/* <div>펫시터 메인페이지(캘린더뷰, 예약 내역 확인)</div> */}
+        <CalendarView />
       </div>
     </>
   );
