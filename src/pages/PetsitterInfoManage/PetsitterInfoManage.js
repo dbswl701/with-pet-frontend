703f75484ca513e53e9bc1462f78520071865dad
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-17 13:02:04 +0900

    ✨ add: 펫시터 정보 수정에서 이용가능 서비스, 필수 서비스 추가

--- src/pages/PetsitterInfoManage/PetsitterInfoManage.js
+++ src/pages/PetsitterInfoManage/PetsitterInfoManage.js
@@ -1,25 +1,59 @@
 import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 
-// function Item({ service }) {
-//   return (
-//     <>
-//       <div style={{ backgroundColor: 'gray' }}>
-//         {/* 사진, 이름, 내용 */}
-//         <img src={service.serviceImg} alt="서비스 이미지" />
-//         <p>{service.serviceName}</p>
-//         <p>{service.serviceIntroduction}</p>
-//       </div>
-//     </>
-//   );
-// }
+function Item1({ service, onRemove }) {
+  // 1. 활성화 (isInclude === true)
+  const includeed = (
+    <div style={{
+      cursor: 'pointer', backgroundColor: 'pink', width: '300px', marginRight: '20px',
+    }}
+    >
+      {/* 사진, 이름, 내용, 가격, 삭제 버튼 */}
+      <img src={service.serviceImg} alt="서비스 이미지" style={{ width: '100px', height: '100px' }} />
+      <p>{service.serviceName}</p>
+      <p>{service.serviceIntroduction}</p>
+      <p>가격: {service.price}</p>
+      <input type="button" value="삭제" onClick={() => onRemove(service.serviceId)} />
+    </div>
+  );
+
+  return (
+    <>
+      { includeed }
+    </>
+  );
+}
+
+function Item2({ service, onAdd }) {
+  const [price, setPrice] = useState(0);
+  // 2. 비활성화 (isInclude === false)
+  const notIncluded = (
+    <div
+      style={{
+        cursor: 'pointer', backgroundColor: 'gray', width: '300px', marginRight: '20px',
+      }}
+    >
+      {/* 사진, 이름, 내용, 가격, 삭제 버튼 */}
+      <img src={service.serviceImg} alt="서비스 이미지" style={{ width: '100px', height: '100px' }} />
+      <p>{service.serviceName}</p>
+      <p>{service.serviceIntroduction}</p>
+      <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
+      <input type="button" value="추가" onClick={() => onAdd(service.serviceId, price)} />
+    </div>
+  );
+
+  return (
+    <>
+      { notIncluded }
+    </>
+  );
+}
 
 function PetsitterInfoManage() {
   const [info, setInfo] = useState({});
-  const [toggle, onToggle] = useState(false);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // 해시태그(petSitterHashTags)
   const [hashTags, setHashTags] = useState([]);
@@ -35,10 +69,10 @@ function PetsitterInfoManage() {
   const [houseImgList, setHouseImgList] = useState([]);
 
   // // 펫시텃 서비스
-  // const [serviceSelectList, setServiceSelectList] = useState([]);
+  const [serviceSelectList, setServiceSelectList] = useState([]);
 
-  // 관리자 서비스
-  // const [withPetServices, setWithPetServices] = useState([]);
+  // 필수 서비스
+  const [criticalServices, setCriticalServices] = useState([]);
   useEffect(() => {
     axios.get('https://withpet.site/api/v1/petsitter/show-myinfo', { withCredentials: true })
       .then((res) => {
@@ -99,26 +133,14 @@ function PetsitterInfoManage() {
         setIntroduction(res.data.result.introduction);
         setHashTags(res.data.result.petSitterHashTags);
         setHouseImgList(res.data.result.petSitterHouses);
-        // setWithPetServices(res.data.result.withPetServices);
+        setCriticalServices(res.data.result.petSitterCriticalServices);
+        setServiceSelectList(res.data.result.petSitterServices);
       })
       .catch(() => {
       });
   }, []);
-  // const nextId = useRef(1);
   console.log(info);
   console.log(info.withPetServices);
-
-  // const onModify = () => {
-  //   navigate('./modify');
-  // };
-
-  // const onChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setInfo({
-  //     ...info,
-  //     [name]: value,
-  //   });
-  // };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -132,20 +154,10 @@ function PetsitterInfoManage() {
     console.log(hashTags);
     const updatedInfo = {
       introduction,
-      petSitterCriticalServiceRequests: [
-        {
-          price: 0,
-          serviceId: 1,
-        },
-      ],
+      petSitterCriticalServiceRequests: criticalServices,
       petSitterHashTagRequests: hashTags,
       petSitterHouseRequests: houseList,
-      petSitterServiceRequests: [
-        {
-          price: 0,
-          serviceIds: 1,
-        },
-      ],
+      petSitterServiceRequests: serviceSelectList,
     };
     console.log(updatedInfo);
     axios.put('https://withpet.site/api/v1/petsitter/update-myinfo', updatedInfo, { withCredentials: true })
@@ -184,26 +196,81 @@ function PetsitterInfoManage() {
   };
   console.log(houseImgList);
 
+  const [isServiceIdIncluded, setIsServiceIdIncluded] = useState([]);
+  const [isCriticalServiceIdIncluded, setIsCriticalServiceIdIncluded] = useState([]);
+
+  // 이용 가능 서비스
+  useEffect(() => {
+    const includedServices = info.withPetServices && info.withPetServices.map((service) => {
+      const isIncluded = serviceSelectList.some((sitterService) => sitterService.serviceId === service.serviceId);
+      const selected = serviceSelectList.find((sitterService) => sitterService.serviceId === service.serviceId);
+      const price = selected ? selected.price : null;
+      return { ...service, isIncluded, price };
+    });
+    setIsServiceIdIncluded(includedServices);
+  }, [serviceSelectList]);
+
+  // 필수 서비스
+  useEffect(() => {
+    const includedServices = info.criticalServices && info.criticalServices.map((service) => {
+      const isIncluded = criticalServices.some((sitterService) => sitterService.serviceId === service.serviceId);
+      const selected = criticalServices.find((sitterService) => sitterService.serviceId === service.serviceId);
+      const price = selected ? selected.price : null;
+      return { ...service, isIncluded, price };
+    });
+    setIsCriticalServiceIdIncluded(includedServices);
+  }, [criticalServices]);
+  console.log(serviceSelectList);
+  // const [newList, setNewList] = useState(isServiceIdIncluded);
+
+  console.log(isServiceIdIncluded);
+  // console.log(newList);
+
+  const onRemoveService = (id) => { // sercieId 건너옴
+    // 활성화된 서비스 삭제 눌렀을 경우
+    console.log(id);
+    setServiceSelectList(serviceSelectList.filter((service) => service.serviceId !== id));
+  };
+
+  const onAddService = (id, price) => { // sercieId 건너옴
+    // 활성화된 서비스 삭제 눌렀을 경우
+    console.log(id);
+    setServiceSelectList([...serviceSelectList, { serviceId: id, price: parseInt(price, 10) }]);
+  };
+
+  const onRemoveCriticalService = (id) => { // sercieId 건너옴
+    // 활성화된 서비스 삭제 눌렀을 경우
+    console.log(id);
+    setCriticalServices(criticalServices.filter((service) => service.serviceId !== id));
+  };
+
+  const onAddCriticalService = (id, price) => { // sercieId 건너옴
+    // 활성화된 서비스 삭제 눌렀을 경우
+    console.log(id);
+    setCriticalServices([...criticalServices, { serviceId: id, price: parseInt(price, 10) }]);
+  };
   const modify = (
     <>
       <p>펫시터 정보 수정 페이지</p>
       <form onSubmit={onSubmit}>
         <p>집사진</p>
         <div>
-          {houseImgList.map((image, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={index}>
-              <img src={image} alt={`Preview ${index}`} style={{ width: '200px', height: '200px', margin: '5px' }} />
-              <input type="button" value="x" />
-              { index === 0 ? <p>대표사진</p> : <p> </p>}
-            </div>
-          ))}
+          {
+            info.petSitterHouses && info.petSitterHouses.map((img, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <div key={index}>
+                <img key={img.houseId} src={img.houseImg} alt="집사진" style={{ width: '200px', height: '200px' }} />
+                <input type="button" value="x" />
+                { index === 0 ? <p>대표사진</p> : <p> </p>}
+              </div>
+            ))
+          }
         </div>
         <input type="file" accept="image/*" multiple onChange={handleImageChange} />
 
         <p>해시태그</p>
         {hashTags && hashTags.map((tag) => (
-          <div key={tag.hashTagName}>
+          <div key={tag.hashTagName} style={{ display: 'flex', flexDirection: 'row' }}>
             <p>{tag.hashTagName}</p>
             <input type="button" value="x" onClick={() => onRemoveHashtag(tag.petSitterHashTagId)} />
           </div>
@@ -219,22 +286,27 @@ function PetsitterInfoManage() {
         <img id="preview-image" alt="이미지 미리보기" src={info.petSitterLicenseImg} />
 
         <p>이용 가능 서비스</p>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          {isServiceIdIncluded && isServiceIdIncluded.map((service) => (service.isIncluded ? (
+            <Item1 key={service.serviceId} service={service} onRemove={onRemoveService} />
+          ) : (
+            <Item2 key={service.serviceId} service={service} onAdd={onAddService} />
+          )))}
+        </div>
+
+        <p>필수 서비스</p>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          {isCriticalServiceIdIncluded && isCriticalServiceIdIncluded.map((service) => (service.isIncluded ? (
+            <Item1 key={service.serviceId} service={service} onRemove={onRemoveCriticalService} />
+          ) : (
+            <Item2 key={service.serviceId} service={service} onAdd={onAddCriticalService} />
+          )))}
+        </div>
         <input className="pet-add-btn" type="submit" value="수정" />
-        <input className="pet-add-btn pet-add-cancel-btn" type="button" value="취소" onClick={() => onToggle(!toggle)} />
+        <input className="pet-add-btn pet-add-cancel-btn" type="button" value="취소" onClick={() => navigate('../petsitterShowInfo')} />
       </form>
     </>
   );
-
-  // const isServiceIdIncluded = info.withPetServices.map((service) => {
-  //   return {
-  //     ...service,
-  //     isIncluded: info.petSitterServices.some(
-  //       (sitterService) => sitterService.serviceId === service.serviceId,
-  //     ),
-  //   };
-  // });
-
-  // console.log(isServiceIdIncluded);
   return (
     <>
       { modify }
