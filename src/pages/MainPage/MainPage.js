f2fae63363f9a292f75604c4cd34b34229bbc8f4
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-19 19:27:54 +0900

    💡 comment: 필요없는 주석 제거

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -13,11 +13,6 @@ import dogBanner from '../../assets/dog_banner.png';
 import MultipleSelectChip from './OptionList';
 import RenderGroup from './Region';
 import MediaCardGrid from './MediaCardGrid';
-// import house1 from '../../assets/house1.png';
-// import house2 from '../../assets/house2.png';
-// import house3 from '../../assets/house3.png';
-// import house4 from '../../assets/house4.png';
-// import house5 from '../../assets/house5.png';
 import PetSize from './PetSize';
 
 const BannerBox = styled.div`
@@ -43,69 +38,6 @@ box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.04);
 border-radius: 5px;
 top: -50px
 `;
-
-// const petSitterList = [
-//   {
-//     id: 1,
-//     house: house1,
-//     name: '한놈',
-//     rate: 4.5,
-//   },
-//   {
-//     id: 2,
-//     house: house2,
-//     name: '두식이',
-//     rate: 3.5,
-//   },
-//   {
-//     id: 3,
-//     house: house3,
-//     name: '석삼',
-//     rate: 4.0,
-//   },
-//   {
-//     id: 4,
-//     house: house4,
-//     name: '너구리',
-//     rate: 2.5,
-//   },
-//   {
-//     id: 5,
-//     house: house5,
-//     name: '오징어',
-//     rate: 5.5,
-//   },
-//   {
-//     id: 6,
-//     house: house1,
-//     name: '한놈',
-//     rate: 4.5,
-//   },
-//   {
-//     id: 7,
-//     house: house2,
-//     name: '두식이',
-//     rate: 3.5,
-//   },
-//   {
-//     id: 8,
-//     house: house3,
-//     name: '석삼',
-//     rate: 4.0,
-//   },
-//   {
-//     id: 9,
-//     house: house4,
-//     name: '너구리',
-//     rate: 2.5,
-//   },
-//   {
-//     id: 10,
-//     house: house5,
-//     name: '오징어',
-//     rate: 5.5,
-//   },
-// ];
 
 function MainPage() {
   const [temp, setTemp] = useState([]);
