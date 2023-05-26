839da1971f4663a102833305411f7057563dff5b
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-26 18:47:20 +0900

    ✨ feat: 펫시터 일지 확인, 수정, 리스트 추가

--- src/pages/PetsitterDiaries/Diary.js
+++ src/pages/PetsitterDiaries/Diary.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+// import PetModify from './PetModify';
+import DiaryDetail from './DiaryDetail';
+import DiaryModify from './DiaryModify';
 
-function Diary({ item, setToggle }) {
-  return (
-    <div style={{ border: '1px solid black' }}>
-      <p>item</p>
-      <p>{item.name}</p>
-      <p>{item.date}</p>
-      <p>{item.category}</p>
-      <p>{item.title}</p>
+function Pet({ pet, onSubmitModify }) {
+  const [toggle, setToggle] = useState('simple');
+  const simple = (
+    <>
+      <img className="pet-img" src={pet.img} alt="반려견 프로필 사진" />
+      <p>{pet.name} / {pet.category} / {pet.title}</p>
       <ExpandCircleDownIcon className="down-icon" fontSize="large" onClick={() => setToggle('detail')} />
+    </>
+  );
+
+  const onToggle = (state) => {
+    setToggle(state);
+  };
+
+  let print = simple;
+
+  switch (toggle) {
+    case 'detail':
+      print = <DiaryDetail pet={pet} onToggle={onToggle} />;
+      break;
+    case 'modify':
+      print = <DiaryModify petInfo={pet} onSubmit={onSubmitModify} onToggle={onToggle} />;
+      break;
+    case 'simple':
+      print = simple;
+      break;
+    default:
+      print = simple;
+      break;
+  }
+  return (
+    <div className={`${toggle === 'simple' ? 'pet-block' : 'pet-detail'}`}>
+      { print }
     </div>
   );
 }
 
-export default Diary;
+export default Pet;
