8f292350f970acdae929b2a60a5e0c8a16fb79d1
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-31 00:14:43 +0900

    💄 design: 반려인 일지 확인 css 수정

--- src/pages/UserDiary/UserDiaryList.js
+++ src/pages/UserDiary/UserDiaryList.js
@@ -2,89 +2,14 @@ import React from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-// import dayjs from 'dayjs';
 import axios from 'axios';
 import UserDiary from './UserDiary';
-// import UserDiaryAdd from './UserDiaryAdd';
 import './Diaries.css';
-// function UserDiaryList(props)
+
 function UserDiaryList({
   open, setOpen, diaries, setDiaries,
 }) {
-  // const [open, setOpen] = useState(false);
-  // const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
-  // const [diaries, setDiaries] = useState([]);
-  // const dateNow = new Date();
-  // const today = dateNow.toISOString().substr(0, 10);
-  // const [diaryInfo, setDiaryInfo] = useState({
-  //   categoryId: 1,
-  //   contentBody: '',
-  //   createdAt: dayjs(today),
-  //   dogId: 1,
-  //   dogImgToday: '',
-  //   title: '',
-  // });
-
-  // setDay(props);
-  // setDogId(props);
-
-  // const onChangetemp = (e) => {
-  //   if (e.target.files) {
-  //     const file = e.target.files[0];
-  //     const reader = new FileReader();
-  //     reader.readAsDataURL(file);
-  //     reader.onloadend = () => {
-  //       setDiaryInfo({
-  //         ...diaryInfo,
-  //         dogImgToday: reader.result,
-  //       });
-  //     };
-  //   } else {
-  //     const { value, name } = e.target;
-  //     setDiaryInfo({
-  //       ...diaryInfo,
-  //       [name]: value,
-  //     });
-  //   }
-  // };
-
-  // const onSubmitAdd = (e) => {
-  //   e.preventDefault();
-  //   const diary = {
-  //     categoryId: diaryInfo.categoryId,
-  //     contentBody: diaryInfo.contentBody,
-  //     createdAt: diaryInfo.createdAt,
-  //     dogId: diaryInfo.dogId,
-  //     dogImgToday: diaryInfo.dogImgToday,
-  //     title: diaryInfo.title,
-  //   };
-  //   console.log(diary);
-  //   axios
-  //     .post('https://withpet.site/api/v1/userdiaries', diary, {
-  //       withCredentials: true,
-  //     })
-  //     .then((res) => {
-  //       setDiaries(diaries.concat(res.data.result));
-  //       console.log(res.data.result);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  //   setDiaryInfo({
-  //     categoryId: 1,
-  //     contentBody: '',
-  //     createdAt: '',
-  //     dogId: 1,
-  //     dogImgToday: '',
-  //     title: '',
-  //   });
-  // };
-  // console.log(dayInfo);
-
   const onSubmitModify = (id, modifyDiaryInfo) => {
-    // setPets(pets.map((pet) => (pet.id === id ? modifyPetInfo : pet)));
-    // const diaryRequest = modifyDiaryInfo;
     axios
       .put(`https://withpet.site/api/v1/userdiaries/${id}`, modifyDiaryInfo, {
         withCredentials: true,
@@ -96,33 +21,19 @@ function UserDiaryList({
           }
           return diary;
         });
-        setDiaries(updatedDiaries); // 새로고침 없이 렌더링 됨
+        setDiaries(updatedDiaries);
       })
       .catch(() => {});
   };
 
-  // const onCancel = () => {
-  //   setDiaryInfo({
-  //     categoryId: '',
-  //     contentBody: '',
-  //     createdAt: '',
-  //     dogId: '',
-  //     dogImgToday: '',
-  //     title: '',
-  //   });
-  // };
-
   return (
     <div>
-      {/* <button style={{ width: '256px' }} onClick={() => setOpen(true)}>일지 작성</button> */}
-      {/* <Button variant="contained" onClick={() => setOpen(true)}>
-        작성
-      </Button> */}
       <Modal open={open} onClose={() => setOpen(false)} style={{ margin: '40px' }}>
         <Box
           sx={{
             width: 800,
-            maxHeight: '80vh',
+            height: 550,
+            // maxHeight: '80vh',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'top',
@@ -130,22 +41,15 @@ function UserDiaryList({
             bgcolor: 'background.paper',
             boxShadow: 24,
             margin: 'auto',
-            overflow: 'scroll',
+            overflowY: 'scroll',
             p: 2,
+            backgroundColor: '#FAF6F0',
           }}
         >
           <div className="diary_container">
             {diaries.map((diary) => {
               return <UserDiary key={diary.userDiaryId} diary={diary} onSubmitModify={onSubmitModify} />;
             })}
-            {/* <UserDiaryAdd
-              onSubmit={onSubmitAdd}
-              onChange={onChangetemp}
-              diaryInfo={diaryInfo}
-              onCancel={onCancel}
-              setDiaryInfo={setDiaryInfo}
-            /> */}
-            {/* 여기에 UserDiaryAdd */}
           </div>
           <Button type="button" onClick={() => setOpen(false)}>
             닫기
