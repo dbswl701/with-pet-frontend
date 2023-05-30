d92ee78cc0cc5daf351b6811e8527039c5028912
KangYunJi <dbswl701@ajou.ac.kr>
2023-05-31 00:31:09 +0900

    💄 design: 반려인 일지 수정 css

--- src/pages/UserDiary/UserDiaryModify.js
+++ src/pages/UserDiary/UserDiaryModify.js
@@ -5,8 +5,9 @@ import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import FormControl from '@mui/material/FormControl';
+// import FormControl from '@mui/material/FormControl';
 import axios from 'axios';
+import MenuItem from '@mui/material/MenuItem';
 
 function UserDiaryModify({ onSubmit, diaryInfo, onToggle }) {
   const [modifyDiaryInfo, setModifyDiaryInfo] = useState({
@@ -20,20 +21,20 @@ function UserDiaryModify({ onSubmit, diaryInfo, onToggle }) {
   });
   const [categories, setCategories] = useState([]);
   const [dogs, setDogs] = useState([]);
-  const styles = {
-    formControl: {
-      margin: '8px',
-      minWidth: '120px',
-    },
-    select: {
-      padding: '8px',
-      fontSize: '16px',
-      border: '1px solid #ccc',
-      borderRadius: '4px',
-      backgroundColor: '#fff',
-      outline: 'none',
-    },
-  };
+  // const styles = {
+  //   formControl: {
+  //     margin: '8px',
+  //     minWidth: '120px',
+  //   },
+  //   select: {
+  //     padding: '8px',
+  //     fontSize: '16px',
+  //     border: '1px solid #ccc',
+  //     borderRadius: '4px',
+  //     backgroundColor: '#fff',
+  //     outline: 'none',
+  //   },
+  // };
 
   useEffect(() => {
     // 반려견 selectbox 불러오기
@@ -91,7 +92,7 @@ function UserDiaryModify({ onSubmit, diaryInfo, onToggle }) {
       title: modifyDiaryInfo.title,
       dogImgToday: modifyDiaryInfo.dogImgToday,
     };
-    // console.log(updatedSubmitInfo);
+    console.log(updatedSubmitInfo);
     onSubmit(diaryInfo.userDiaryId, updatedSubmitInfo);
   };
 
@@ -106,100 +107,42 @@ function UserDiaryModify({ onSubmit, diaryInfo, onToggle }) {
   };
   const modify = (
     <form onSubmit={onLocalSubmit}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          sx={{ m: 1 }}
-          label="날짜"
-          value={dayjs(modifyDiaryInfo.createdAt)}
-          onChange={onChangeCalendar}
-          name="createdAt"
-          format="YYYY/MM/DD"
-        />
-      </LocalizationProvider>
-      <FormControl
-        sx={{ m: 1, minWidth: 120 }}
-        size="small"
-        name="categoryId"
-        value={modifyDiaryInfo.categoryId}
-        onChange={onChange}
-      >
-        {/* <InputLabel id="demo-simple-select-label">카테고리</InputLabel> */}
-        <select
-          name="categoryId"
-          id="categoryId"
-          value={modifyDiaryInfo.categoryId}
-          label="카테고리"
-          style={styles.select}
-          onChange={onChange}
-        >
-          {categories.map((item) => (
-            <option
-              key={item.categoryId}
-              value={item.categoryId}
-            >
-              {item.name}
-            </option>
-          ))}
-        </select>
-      </FormControl>
-      <FormControl
-        sx={{ m: 1, minWidth: 120 }}
-        size="small"
-      >
-        {/* <InputLabel id="demo-simple-select-label">반려견</InputLabel> */}
-        <select
-          name="dogId"
-          id="dogId"
-          value={modifyDiaryInfo.dogId}
-          label="반려견"
-          style={styles.select}
-          onChange={onChange}
-        >
-          {dogs.map((item) => (
-            <option key={item.dogId} value={item.dogId}>
-              {item.name}
-            </option>
-          ))}
-        </select>
-      </FormControl>
-      <div className="select-diary-type">
-        <img id="preview-img" src={modifyDiaryInfo.dogImgToday} alt="preview" style={{ width: '100px', height: '100px' }} />
-        <label htmlFor="image-select">오늘의 사진 선택</label>
-        <input
-          type="file"
-          accept="image/*"
-          id="image-select"
-          style={{ display: 'none' }}
-          onChange={onChange}
-        />
-      </div>
-      <div className="diary-contents-regist">
-        {/* 카테고리 바꾸기 */}
-        <TextField
-          sx={{ m: 1 }}
-          label="제목"
-          variant="outlined"
-          size="small"
-          name="title"
-          onChange={onChange}
-          value={modifyDiaryInfo.title}
-        />
-        <TextField
-          sx={{ m: 1 }}
-          label="내용"
-          variant="outlined"
-          size="medium"
-          name="contentBody"
-          onChange={onChange}
-          value={modifyDiaryInfo.contentBody}
-        />
-        <input className="diary-add-btn" type="submit" value="수정" />
-        <input
-          className="diary-add-btn diary-add-cancel-btn"
-          type="button"
-          value="취소"
-          onClick={() => onToggle('detail')}
-        />
+      <div style={{ display: 'flex', flexDirection: 'column', marginTop: '30px' }}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div>
+            <div>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker sx={{ m: 1 }} label="날짜" value={dayjs(modifyDiaryInfo.createdAt)} onChange={onChangeCalendar} name="createdAt" format="YYYY/MM/DD" slotProps={{ textField: { size: 'small', length: 'small' } }} />
+              </LocalizationProvider>
+            </div>
+            <div>
+              <TextField sx={{ m: 1 }} select label="반려견 선택" variant="outlined" name="dogId" style={{ width: '132px' }} onChange={onChange} value={modifyDiaryInfo.dogId} size="small" required>
+                { dogs.map((item) => <MenuItem key={item.dogId} value={item.dogId}>{item.name}</MenuItem>)}
+              </TextField>
+              <TextField sx={{ m: 1 }} select label="카테고리 선택" variant="outlined" name="categoryId" style={{ width: '132px' }} onChange={onChange} value={modifyDiaryInfo.categoryId} size="small" required>
+                { categories.map((item) => <MenuItem key={item.categoryId} value={item.categoryId}>{item.name}</MenuItem>)}
+              </TextField>
+            </div>
+
+            <div>
+              <TextField sx={{ m: 1 }} label="제목" value={modifyDiaryInfo.title} style={{ width: '282px' }} variant="outlined" size="small" name="title" onChange={onChange} />
+            </div>
+          </div>
+          <div style={{ marginLeft: '50px' }}>
+            <div className="today-img-regist" style={{ display: 'flex', flexDirection: 'column' }}>
+              <label htmlFor="image-select">
+                <img style={{ width: '150px', height: '150px', border: '1px solid gray' }} alt="이미지 미리보기" src={diaryInfo.dogImgToday} />
+              </label>
+              <input type="file" accept="image/*" id="image-select" style={{ display: 'none' }} onChange={onChange} />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <TextField sx={{ m: 1 }} label="내용" value={modifyDiaryInfo.contentBody} multiline rows={6} style={{ width: '500px' }} variant="outlined" size="small" name="contentBody" onChange={onChange} />
+          </div>
+        </div>
+        <input className="diary-add-btn" type="submit" value="저장" style={{ width: '510px' }} />
       </div>
     </form>
   );
