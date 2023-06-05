271cc7bbc6f7334d97b5fbbd5aa0c1b279d28c28
2jaeyoung2 <joe981125@ajou.ac.kr>
2023-06-05 14:54:42 +0900

    ✨	feat: 알림창(notification.js) 형식 추가

---
+++ src/pages/notification.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function SimpleSnackbar() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      <Button onClick={handleClick}>Open simple snackbar</Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="내용"
+        sx={{
+          "& .MuiSnackbarContent-root": {
+            border: "2px solid #CAA969", // 테두리
+            backgroundColor: "#FAF6F0", // 배경색
+            color: "#000000", // 글자 색
+          },
+        }}
+        action={action}
+      />
+    </div>
+  );
+}
