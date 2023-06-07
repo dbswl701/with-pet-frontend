867776f24f4845c1f0a9ec41df7f66790e6b84f6
KangYunJi <dbswl701@ajou.ac.kr>
2023-06-07 11:47:52 +0900

    ➕ add: 알림페이지 알림 하나 클릭 시 페이지 이동 추가

--- src/pages/Notification/NotificationPage.js
+++ src/pages/Notification/NotificationPage.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 function Item({ noti }) {
   const date = noti.createdAt.split('T')[0];
+  // const [toggle, setToggle] = useState(false);
+  const navigate = useNavigate();
   return (
-    <div style={{
-      justifyContent: 'center', marginBottom: '8px', alignItems: 'center', display: 'flex', flexDirection: 'row', width: '600px', height: '45px', backgroundColor: '#FAF6F0', border: '1px solid #CAA969', borderRadius: '10px',
-    }}
+    <div
+      // onMouseEnter={() => setToggle(true)}
+      // onMouseLeave={() => setToggle(false)}
+      onClick={() => navigate(noti.url)}
+      style={{
+        cursor: 'pointer', justifyContent: 'space-evenly', marginBottom: '8px', alignItems: 'center', display: 'flex', flexDirection: 'row', width: '600px', height: '45px', backgroundColor: !noti.isRead ? '#FAF6F0' : 'white', border: '1.5px solid #CAA969', borderRadius: '10px',
+      }}
     >
-      <p>{noti.notificationType}</p>
+      <p style={{ fontWeight: 'bold' }}>{noti.notificationType}</p>
       <p style={{ fontSize: '13px' }}>{noti.content}</p>
-      <p>{date}</p>
+      <p style={{ fontSize: '13px' }}>{date}</p>
     </div>
   );
 }
@@ -23,11 +30,16 @@ function NotificationPage() {
       });
   }, []);
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      알림 페이지
-      { notiList && notiList.map((item) => {
-        return <Item key={item.notificationId} noti={item} />;
-      })}
+    <div style={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center',
+    }}
+    >
+      <p style={{ fontSize: '48px', fontWeight: 'bold', marginBottom: '20px' }}>알림</p>
+      <div style={{ marginTop: '17px' }}>
+        { notiList && notiList.map((item) => {
+          return <Item key={item.notificationId} noti={item} />;
+        })}
+      </div>
     </div>
   );
 }
