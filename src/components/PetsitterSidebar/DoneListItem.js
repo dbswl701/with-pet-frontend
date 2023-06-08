3ca1e1e3f8d6ea5cfcb88a09b58563b38079ec7f
KangYunJi <dbswl701@ajou.ac.kr>
2023-06-08 13:12:55 +0900

    ➕ add: 펫시터 사이드바 이용완료 반려인 사회화 온도 추가

--- src/components/PetsitterSidebar/DoneListItem.js
+++ src/components/PetsitterSidebar/DoneListItem.js
@@ -51,6 +51,13 @@ function DoneListItem({ item, setPrintBody }) {
               </Progress>
               <p className="social">{item.socializationTemperature}%</p>
             </EvalContainer>
+            <EvalContainer>
+              <IconImg className="social" src={social} alt="social" />
+              <Progress className="social">
+                <Dealt className="social" dealt={item.socializationDegree} />
+              </Progress>
+              <p className="social">{item.socializationDegree}%</p>
+            </EvalContainer>
           </BarContainer>
         </div>
         <div>
