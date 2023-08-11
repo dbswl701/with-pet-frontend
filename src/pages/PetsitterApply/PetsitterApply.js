5fada6ab941b57247853bb40b4adcf9aa7d708d2
KangYunJi <dbswl701@ajou.ac.kr>
2023-08-11 20:39:06 +0900

    💄 design: 펫시터 지원 radio 버튼 UI 수정

--- src/pages/PetsitterApply/PetsitterApply.js
+++ src/pages/PetsitterApply/PetsitterApply.js
@@ -14,6 +14,33 @@ import camera from '../../assets/camera.png';
 const Container = styled.div`
   border: 1px solid #999999;
   width: 1500px;
+  padding: 100px 150px;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  cursor: pointer;
+  height: 48px;
+  width: 211px;
+  border: 2px solid white;
+  border-radius: 5px;
+  text-align: center;
+  vertical-align: center;
+  line-height: 36px;
+  padding: 5px;
+  margin-right: 30px;
+  background-color: white;
+
+  &: hover {
+    background-color: rgb(212, 212, 212);
+    border: 2px solid rgb(212, 212, 212);
+  }
+
+  input[type="radio"]:checked + & {
+    background-color: #CAA969;
+    color: white
+  }
+
 `;
 
 function PetsitterApply() {
@@ -82,32 +109,40 @@ function PetsitterApply() {
       <FormContainer onSubmit={onSubmit}>
         <Typography component="h2" variant="h6" color="primary" gutterBottom sx={{ color: '#caa969' }} align="left">1. 기본 정보</Typography>
         <Container>
-          <p>1. 생년월일</p>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker value={info.applicantBirth} onChange={(date) => onChange(date, 'birth')} name="applicantBirth" format="YYYY/MM/DD" />
-          </LocalizationProvider>
-          <div className="select">
-            <p>2. 흡연 여부</p>
-            <input
-              type="radio"
-              name="applicantIsSmoking"
-              id="O"
-              value="true"
-              onChange={onChange}
-              checked={info.applicantIsSmoking === 'true'}
-            />
-            <label htmlFor="O">O</label>
-            <input
-              type="radio"
-              name="applicantIsSmoking"
-              id="X"
-              value="false"
-              onChange={onChange}
-              checked={info.applicantIsSmoking === 'false'}
-            />
-            <label htmlFor="X">X</label>
+          <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '100px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <p>1. 생년월일</p>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker value={info.applicantBirth} onChange={(date) => onChange(date, 'birth')} name="applicantBirth" format="YYYY/MM/DD" />
+              </LocalizationProvider>
+            </div>
+
+            <div style={{ marginLeft: '437px' }}>
+              <p>2. 흡연 여부</p>
+              <input
+                type="radio"
+                name="applicantIsSmoking"
+                id="O"
+                value="true"
+                onChange={onChange}
+                checked={info.applicantIsSmoking === 'true'}
+                style={{ display: 'none' }}
+              />
+              <Label htmlFor="O" style={{ border: '1px solid #CAA969' }}>흡연</Label>
+              <input
+                type="radio"
+                name="applicantIsSmoking"
+                id="X"
+                value="false"
+                onChange={onChange}
+                checked={info.applicantIsSmoking === 'false'}
+                style={{ display: 'none' }}
+              />
+              <Label htmlFor="X" style={{ border: '1px solid #CAA969' }}>비흡연</Label>
+            </div>
           </div>
-          <div className="select">
+
+          <div>
             <p>3. 성별</p>
             <input
               type="radio"
@@ -116,8 +151,9 @@ function PetsitterApply() {
               value="female"
               onChange={onChange}
               checked={info.applicantGender === 'female'}
+              style={{ display: 'none' }}
             />
-            <label htmlFor="female">여성</label>
+            <Label htmlFor="female" style={{ border: '1px solid #CAA969' }}>여성</Label>
             <input
               type="radio"
               name="applicantGender"
@@ -125,13 +161,14 @@ function PetsitterApply() {
               value="male"
               onChange={onChange}
               checked={info.applicantGender === 'male'}
+              style={{ display: 'none' }}
             />
-            <label htmlFor="male">남성</label>
+            <Label htmlFor="male" style={{ border: '1px solid #CAA969' }}>남성</Label>
           </div>
         </Container>
         <Typography component="h2" variant="h6" color="primary" gutterBottom sx={{ color: '#caa969' }} align="left">2. 반려 경험 및 경력</Typography>
         <Container>
-          <div className="select">
+          <div>
             <p>1. 강아지 반려 경험 유무</p>
             <input
               type="radio"
@@ -140,8 +177,9 @@ function PetsitterApply() {
               value="true"
               onChange={onChange}
               checked={info.applicantHavingWithPet === 'true'}
+              style={{ display: 'none' }}
             />
-            <label htmlFor="having">O</label>
+            <Label htmlFor="having" style={{ border: '1px solid #CAA969' }}>O</Label>
             <input
               type="radio"
               name="applicantHavingWithPet"
@@ -149,8 +187,9 @@ function PetsitterApply() {
               value="false"
               onChange={onChange}
               checked={info.applicantHavingWithPet === 'false'}
+              style={{ display: 'none' }}
             />
-            <label htmlFor="not">X</label>
+            <Label htmlFor="not" style={{ border: '1px solid #CAA969' }}>X</Label>
           </div>
           <TextField sx={{ m: 1 }} label="2. 반려 동물 관련 경력 또는 경험" variant="outlined" name="applicantAnimalCareer" onChange={onChange} value={info.applicantAnimalCareer} required />
         </Container>
