96155100835f3b1cd629b740ddfd549faeadb43f
dbswl701 <dbswl701@ajou.ac.kr>
2024-05-09 13:03:15 +0900

    add: 전화번호 인증번호 재발급 쿠키 저장

--- src/services/sms.js
+++ src/services/sms.js
@@ -4,10 +4,13 @@ import baseUrl from './api';
 // 휴대폰, 인증번호
 const getReissuance = async (phone) => {
   try {
-    const res = await axios.get(`${baseUrl}/v2/sms-authentication?to=${phone}`);
+    const res = await axios.get(`${baseUrl}/v2/sms-authentication?to=${phone}`, { withCredentials: true });
     return res.data.result;
   } catch (err) {
     console.error('전화번호 인증 번호 발급 실패', err);
+    if (err.response && err.response.status === 400) {
+      alert(err.response.data.message);
+    }
     if (err.response && err.response.status === 409) {
       alert(err.response.data.message);
     }
