94b5a0b265a84c0a9d11c05c400f32a787b9f38a
dbswl701 <dbswl701@ajou.ac.kr>
2024-05-24 00:05:54 +0900

    style: 관리자 펫시터 지원자 상세정보 페이지 UI 수정

--- src/pages/AdminApplicantDetail/ApplicantDetail.js
+++ src/pages/AdminApplicantDetail/ApplicantDetail.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-alert */
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import { getAdminApplicantDetail, postAdminApplicantApprove, postAdminApplicantRefuse } from '../../services/admin';
 import * as S from './ApplicantDetail.styles';
 
@@ -21,17 +21,16 @@ function ApplicantDetail() {
   const handleApprove = async () => {
     try {
       await postAdminApplicantApprove(id);
-      // eslint-disable-next-line no-alert
       alert('승인되었습니다.');
       navigate(-1);
     } catch (err) {
       console.error(err);
     }
   };
+
   const handleCancle = async () => {
     try {
       await postAdminApplicantRefuse(id);
-      // eslint-disable-next-line no-alert
       alert('거절되었습니다.');
       navigate(-1);
     } catch (err) {
@@ -42,64 +41,38 @@ function ApplicantDetail() {
   return (
     <S.Container>
       <S.Title>지원자 상세정보</S.Title>
-      <div style={{
-        display: 'flex', flexDirection: 'row', width: '600px', alignItems: 'center',
-      }}
-      >
-        <div style={{ marginRight: '30px' }}>
-          <img style={{ width: '76px', height: '76px', borderRadius: '50%' }} src={info.applicantImg} alt="지원자 프로필 사진" />
-        </div>
-        <div>
-          <div style={{
-            display: 'flex', flexDirection: 'row', alignItems: 'center',
-          }}
-          >
-            <p style={{ fontSize: '20px', margin: '0px', marginRight: '10px' }}><b>{info.applicantName}</b></p>
-            <p style={{
-              fontSize: '16px', margin: '0px', color: '#999999', letterSpacing: '-0.05em',
-            }}
-            >{info.applicantStreetAdr}
-            </p>
-          </div>
-          <div>
+      <S.UserInfoContainer>
+        <S.ProfileImg src={info.applicantImg} alt="지원자 프로필 사진" />
+        <S.SubContainer>
+          <S.UserInfoSubContainer>
+            <S.UserName>{info.applicantName}</S.UserName>
+            <S.UserAddr>{info.applicantStreetAdr}</S.UserAddr>
+          </S.UserInfoSubContainer>
+          <S.SubContainer>
             <S.DescriptionSub>흡연 여부: {info.applicantIsSmoking ? 'O' : 'X'}</S.DescriptionSub>
-          </div>
-          <div>
+          </S.SubContainer>
+          <S.SubContainer>
             <S.DescriptionSub>강아지 반려 경험 유무: {info.applicantHavingWithPet ? 'O' : 'X'}</S.DescriptionSub>
-          </div>
-        </div>
-      </div>
-      <div>
+          </S.SubContainer>
+        </S.SubContainer>
+      </S.UserInfoContainer>
+      <S.SubContainer>
         <S.SubTItle>지원 동기</S.SubTItle>
-        <p style={{ fontSize: '14px', color: '#999999' }}>{info.applicantMotivation}</p>
-      </div>
-      <div>
+        <S.DescriptionContent>{info.applicantMotivation}</S.DescriptionContent>
+      </S.SubContainer>
+      <S.SubContainer>
         <S.SubTItle>그 외 반려동물 관련 경력 또는 경험</S.SubTItle>
-        <p style={{ fontSize: '14px', color: '#999999' }}>{info.applicantAnimalCareer}</p>
-      </div>
-      <div>
+        <S.DescriptionContent>{info.applicantAnimalCareer}</S.DescriptionContent>
+      </S.SubContainer>
+      <S.SubContainer>
         <S.SubTItle>자격증</S.SubTItle>
-        <img style={{ height: '300px' }} src={info.applicantLicenseImg} alt="지원자 자격증 사진" />
-      </div>
-      <div style={{
-        display: 'flex', justifyContent: 'center', flexDirection: 'column', marginTop: '30px',
-      }}
-      >
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <S.Button
-            onClick={handleApprove}
-            approve
-          >승낙
-          </S.Button>
-          <S.Button
-            onClick={handleCancle}
-          >거절
-          </S.Button>
-        </div>
-        <Button type="button" onClick={() => navigate(-1)}>
-          <p style={{ fontSize: '20px' }}>돌아가기</p>
-        </Button>
-      </div>
+        <S.LicenseImg src={info.applicantLicenseImg} alt="지원자 자격증 사진" />
+      </S.SubContainer>
+      <S.ButtonWrapper>
+        <S.Button onClick={handleApprove} approve>승낙</S.Button>
+        <S.Button onClick={handleCancle}>거절</S.Button>
+      </S.ButtonWrapper>
+      <S.BackButton onClick={() => navigate(-1)}>돌아가기</S.BackButton>
     </S.Container>
   );
 }
