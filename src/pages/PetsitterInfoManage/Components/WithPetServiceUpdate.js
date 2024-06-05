2a46a8a8a3e2152657e083d44e7b9e9f6d4001f4
dbswl701 <dbswl701@ajou.ac.kr>
2024-06-05 23:45:14 +0900

    comment: 주석 정리

--- src/pages/PetsitterInfoManage/Components/WithPetServiceUpdate.js
+++ src/pages/PetsitterInfoManage/Components/WithPetServiceUpdate.js
@@ -8,6 +8,7 @@ function WithPetServiceUpdate({ isServiceIdIncluded, onRemoveService, onAddServi
     <div>
       <S.Title>이용 가능 서비스</S.Title>
       <S.ServiceList>
+        {/* 컴포넌트 정리 */}
         {/* {isServiceIdIncluded && isServiceIdIncluded.map((service) => (service.isIncluded ? (
           <AddedService key={service.withPetServiceId} isIncluded={service.isIncluded} serviceImg={service.withPetServiceImg} serviceName={service.withPetServiceName} serviceIntroduction={service.withPetServiceIntroduction} price={service.price} serviceId={service.withPetServiceId} onRemove={onRemoveService} />
         ) : (
