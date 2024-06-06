b47067deff38e5bcc725544facd050f8e53e91e8
dbswl701 <dbswl701@ajou.ac.kr>
2024-06-06 23:55:09 +0900

    fix: 가격 나오지 않는 문제 해결

--- src/pages/PetsitterInfoManage/Components/ServiceItem.js
+++ src/pages/PetsitterInfoManage/Components/ServiceItem.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from '../PetsitterInfoManage.styles';
 
 function ServiceItem({
   isIncluded, serviceImg, serviceName, serviceIntroduction, price, serviceId, onRemove, onAdd,
 }) {
   console.log('price:', price);
+  const [priceValue, setPriceValue] = useState(0);
   return (
     <S.AddedServiceContainer isIncluded={isIncluded}>
       <S.ServiceInnerContainer>
@@ -15,12 +16,11 @@ function ServiceItem({
         </S.ServiceIntroContainer>
       </S.ServiceInnerContainer>
       <S.ServicePriceContainer>
-        <S.ServicePriceInput type="number" value={price} disabled={isIncluded} />
+        <S.ServicePriceInput type="number" value={priceValue} disabled={isIncluded} onChange={(e) => setPriceValue(e.target.value)} />
         {
           isIncluded ? <S.ServicePriceBtn type="button" value="삭제" onClick={() => onRemove(serviceId)} />
-            : <S.ServicePriceBtn type="button" value="추가" onClick={() => onAdd(serviceId, price, serviceName)} />
+            : <S.ServicePriceBtn type="button" value="추가" onClick={() => onAdd(serviceId, priceValue, serviceName)} />
         }
-
       </S.ServicePriceContainer>
     </S.AddedServiceContainer>
   );
